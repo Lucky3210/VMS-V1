b3a05f3b1a623811d893b61b594714fb8eec2c21
Lucky3210 <godwinudomg@gmail.com>
2024-04-17 10:46:29 +0100

    Editing Popform

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import Header from './components/Header';
 import Attendant_login from './pages/Attendant_login';
 import Generic_dashboard from './pages/Generic_dashboard';
-import PopupForm from './pages/PopupForm';
 
 function App() {
   return (
     <div className="App">
-      <Attendant_login />
-      {/* <Generic_dashboard /> */}
+      {/* <Attendant_login /> */}
+      <Generic_dashboard />
     </div>
   );
 }
